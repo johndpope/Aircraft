4b78ee1218f95a52ace8252741e6fc8552247d0a
Mark Lee <lizero213@163.com>
2015-06-04 00:37:31 +0800

    Update explosion effects, add loop BGM player

---
+++ Assets/Scripts/BGMController.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+
+@script RequireComponent(AudioSource)
+
+public var BGMClip:AudioClip;
+public var loopStartTime:double;
+public var loopEndTime:double;
+public var isLoop:boolean=false;
+
+private var BGMAudioSource:AudioSource;
+
+function Start () {
+	BGMAudioSource = gameObject.AddComponent.<AudioSource>();
+	BGMAudioSource.playOnAwake = false;
+	BGMAudioSource.clip = BGMClip;
+	
+	BGMAudioSource.loop=false;
+	isLoop = false;
+	
+	StartMusic();
+}
+
+function Update () {
+	if (BGMAudioSource.isPlaying && BGMAudioSource.time >= loopEndTime) {
+		isLoop = true;
+		BGMAudioSource.time = loopStartTime;
+	}
+	
+}
+
+function StartMusic () {
+	BGMAudioSource.time = 0;
+	BGMAudioSource.Play();
+	
+}
