8d285067c2130b2dc762d1ddd4ae08527819f794
Mark Lee <lizero213@163.com>
2015-06-04 17:49:21 +0800

    add new awesome weapon: Beam Magnum Launcher

---
+++ Assets/Game/Scripts/BeamMagnumBullet.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeamMagnumBullet : Entity {
+
+	public GameObject spark;
+	public Rigidbody body;
+	public float lifeTimer;
+	public float lifeTime=6;
+	public float damage=800;
+
+	public void OnTriggerEnter(Collider _collider){
+		Debug.Log ("mega hit "+_collider.name);
+		if (_collider.CompareTag("AttackTarget") ){
+			Entity ent=_collider.GetComponent<Entity>();
+			if (ent.player.flag!=player.flag){
+				Explode();
+				ent.Hurt(damage);
+			}
+		}
+		else{
+			Explode();
+		}
+	}
+	
+	public void Explode(){
+		if (spark!=null){
+			GameObject sparkObj=Instantiate(spark);
+			sparkObj.transform.position=body.position;//transform.position;
+		}
+		Destroy(this.gameObject);
+	}
+	
+	void Awake(){
+		body=GetComponent<Rigidbody>();
+	}
+	
+	void Update(){
+		transform.rotation=Quaternion.LookRotation(body.velocity);
+		if (lifeTimer<lifeTime){
+			lifeTimer+=Time.deltaTime;
+		}
+		else{
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Assets/Game/Scripts/BeamMagnumController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeamMagnumController : WeaponObject {
+
+	public BeamMagnumBullet bulletPrefab;
+	public float speed;
+	public float fireInterval=1.0f;
+	private float fireToggle;
+	
+	public void Fire(){
+		if (fireToggle<fireInterval){
+			return;
+		}
+		fireToggle-=fireInterval;
+		if (bulletPrefab!=null){
+			BeamMagnumBullet bullet=Instantiate(bulletPrefab);
+			//			missile.transform.rotation=transform.rotation;
+			bullet.transform.position=bulletTransform.position+new Vector3(0,0,0);
+			
+			bullet.player=owner.player;
+			bullet.transform.rotation=bulletTransform.rotation;
+			
+			
+			bullet.GetComponent<Rigidbody>().velocity=bulletTransform.forward* speed;
+			
+			this.GetComponent<AudioSource>().Play();
+			
+		}
+	}
+	
+	protected override void Update(){
+		base.Update();
+		
+		if (fireToggle<fireInterval){
+			fireToggle+=Time.deltaTime;
+		}
+		
+		
+		
+		if (Input.GetKey(KeyCode.X)){
+			Fire ();
+
+			
+		}
+		else {
+
+		}
+	}
+}
