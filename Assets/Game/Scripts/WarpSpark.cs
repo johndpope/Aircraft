5b6185a069d8674871c5deef29bf04b18b01965e
Mark Lee <lizero213@163.com>
2015-06-07 21:30:11 +0800

    bug fix

--- Assets/Game/Scripts/WarpSpark.cs
+++ Assets/Game/Scripts/WarpSpark.cs
@@ -7,7 +7,7 @@ public class WarpSpark : MonoBehaviour {
 	public GameObject warpHeatwave;
 	public Light warpLight;
 
-	public float animationTime=0.5f;
+	public float animationTime=0.3f;
 	public float lifeTime=1;
 
 	private float lifeTimer;
@@ -15,8 +15,8 @@ public class WarpSpark : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		warpWave.transform.localScale = new Vector3(0.2f,1,0.2f);
-		warpHeatwave.transform.localScale = new Vector3(0.2f,1,0.2f);
+		warpWave.transform.localScale = new Vector3(1f,1,1f);
+		warpHeatwave.transform.localScale = new Vector3(1f,1,1f);
 
 		lifeTimer = 0;
 		animationTimer = 0;
@@ -29,8 +29,8 @@ public class WarpSpark : MonoBehaviour {
 		if (lifeTimer < lifeTime) {
 			lifeTimer+=Time.deltaTime;
 
-			warpWave.transform.localScale = Vector3.Lerp(new Vector3(0.2f,1,0.2f),new Vector3(4,1,4),(animationTime-animationTimer)/animationTime);
-			warpHeatwave.transform.localScale = Vector3.Lerp(new Vector3(0.2f,1,0.2f),new Vector3(4,1,4),(animationTime-animationTimer)/animationTime);
+			warpWave.transform.localScale = Vector3.Lerp(new Vector3(1f,1,1f),new Vector3(4,1,4),(animationTimer)/animationTime);
+			warpHeatwave.transform.localScale = Vector3.Lerp(new Vector3(1f,1,1f),new Vector3(4,1,4),(animationTimer)/animationTime);
 			animationTimer+=Time.deltaTime;
 		}
 		else {
